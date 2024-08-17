6a0a693b8c1bd1d44806ff96058100e750ea848b
xDevansh <devjollani@Gmail.com>
2024-08-18 00:08:59 +0530

    before start

---
+++ pythonProject/learning.py
@@ -0,0 +1,17 @@
+#print welcome message
+message1="'hello\"s world'"
+print(message1)
+message2="""Hello
+World
+
+"""
+print(message2)
+print(len(message1))
+print(message1[-1])
+print(message1[:6])
+print(message1[9:])
+#using methods
+
+print(message1.upper())
+print(message1.count('o'))
+print(message1.find('o'))
