932c85c6d9fdb79648e14a283c4baf7e1a8389c4
xDevansh <devjollani@Gmail.com>
2024-08-18 10:33:27 +0530

    before start

--- pythonProject/strings.py
+++ pythonProject/strings.py
@@ -15,3 +15,14 @@ print(message1[9:])
 print(message1.upper())
 print(message1.count('o'))
 print(message1.find('o'))
+print(message1.replace('"','is'))
+
+msg1='hello'
+msg2='dev'
+print(msg1+' '+msg2)
+print('{} {}'.format(msg1,msg2))
+
+print(dir(str))
+print(help(str.strip))
+msg3= '...hello dev...'
+print(msg3.strip('.' )) #only from beginning and end
