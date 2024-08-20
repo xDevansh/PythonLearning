9835cc3157ded756b8b7138b47f36c257a02e6f2
xDevansh <devjollani@Gmail.com>
2024-08-21 02:35:38 +0530

    during

--- pythonProject/import_test/my_module_import.py
+++ pythonProject/import_test/my_module_import.py
@@ -1,10 +1,10 @@
-import my_module
+import my_module as mm
 
 courses= ['math','art','science','history','geography']
 
 print('enter the target which you wanna find in the courses list')
 target= input()
-index= my_module.find_index(courses,target)
+index= mm.find_index(courses,target)
 if index==-1:
     print('not found in courses list')
 else:
